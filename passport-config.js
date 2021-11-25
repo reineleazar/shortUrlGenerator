632d33aa4f1acaf20ae29232ee2b97ea87e70af5
reineleazar <89068052+reineleazar@users.noreply.github.com>
2021-11-25 09:23:49 +0800

    Update passport-config.js
    moved code from serverjs to passport-configjs

--- passport-config.js
+++ passport-config.js
@@ -3,59 +3,36 @@ const passport  = require('passport')
 const bcrypt = require('bcrypt')
 const shortUrl = require('./models/shortUrl')
 
-
-    // const authenticateUser = async (email,password,done) =>
-    // {  
-    //     const user = getUserEmail(email)
-    //     {
-    //         if(user==null)
-    //         {
-    //             return done(null,false,{message: 'no user with that email'})
-    //         }
-
-    //         try {
-    //             if(await bcrypt.compare(password,user.password)){
-    //                 return done(null,user)
-    //             }else{
-    //                 return done(null,false,{message:'Password Incorrect'})
-    //             }
-    //         }catch(err){
-    //             return done(err)
-
-    //         }
-    //     }
-    // }
-    // passport.use(new LocalStrategy(function (username, password, done) {
-    //     shortUrl.findOne({ username: username })}),authenticateUser)
-
-
-  module.exports = function(passport){  
-    passport.use(new LocalStrategy({usernameField: 'email'},(email,password,done)=>{
-        shortUrl.findOne({email:email}).then(user=>{if(!user){
+    function initialize(passport,email,_id){
+    const authenticateUser = async(email,password,done)=>{
+        const user = await shortUrl.findOne({email:email})
+        if(!user)
+        {
             return done(null,false,{message: 'no user with that email'})
         }
-        try {
-                        if( bcrypt.compare(password,user.password)){
-                            return done(null,user)
-                        }else{
-                            return done(null,false,{message:'Password Incorrect'})
-                        }
-                    }catch(err){
-                        return done(err)
-        
-                    }
-    })
+        else
+        { 
+            try{
+       
+                if(await bcrypt.compare(password,user.password)){
+                     return done(null,user)
+                 }else{
+                     return done(null,false,{message:'Password Incorrect'})
+                 }}
+                 catch(err){
+                     return done(err)
+                 }
+        }
+    
+    }
+  passport.use(new LocalStrategy({usernameField:'email'},authenticateUser))
+  passport.serializeUser((user,done)=> done(null,user))
+  passport.deserializeUser(function(_id, done){
+  shortUrl.findById(_id, function(err,user){
+      done(err,user)
+  })
+  })
 
+}
 
-    }))
-
-  }  
-
-    passport.serializeUser((user,done)=> done(null,user))
-    passport.deserializeUser((Id,done)=> {
-        return done(null,Id)
-    })
-
-
-
-//module.exports = initialize
+module.exports = initialize
